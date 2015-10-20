8af366ccd32a1e9d1df1c2b54964fa4f4a81f24c
Roland Pallai <pallair@magex.hu>
2015-10-20 02:03:50 +0200

    A diszkret datumalapu lapozas tamogatasa.

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -16,9 +16,8 @@ class ViewController < ApplicationController
         return if needs_deeply_concerned(@treasury)
         return if needs_treasury_supervisor(@treasury) if @account.hidden?
       end
-      where_account_id = @account.id
+      account_ids = @account.id
       @date_field = @account.order_column
-      @order = @account.order_column
       @page_title << '/' << view_context.print_account(@account)
     elsif params[:account_type_code]
       @treasury = Treasury.find(params[:treasury_id])
@@ -27,9 +26,8 @@ class ViewController < ApplicationController
       accounts = accounts.where(subtype_code: params[:account_subtype_code]) if params[:account_subtype_code]
       #return if needs_treasury_supervisor(@treasury) if accounts.any?(&:hidden?)
       # az .ids nem mukodik, valami Rails bug
-      where_account_id = accounts.pluck('accounts.id')
+      account_ids = accounts.pluck('accounts.id')
       @date_field = 'transactions.date'
-      @order = @date_field
       @page_title << '/accounts:' << params[:account_type_code]
       @page_title << '|' << params[:account_subtype_code] if params[:account_subtype_code]
     elsif params[:person_id]
@@ -39,51 +37,82 @@ class ViewController < ApplicationController
         return if needs_deeply_concerned(@treasury)
       end
       accounts = @person.accounts_for_equity
-      where_account_id = accounts.ids
+      account_ids = accounts.ids
       @date_field = 'transactions.date'
-      @order = @date_field
       @page_title << '/' << @person.name
     else
       raise "WAT?"
     end
 
-    operations = Operation.where(account_id: where_account_id).
+    @order = @date_field
+    @order << " DESC" if @backward
+
+    operations = Operation.where(account_id: account_ids).
       joins(:title => :transaction).
       group('titles.id').
-      order(@order, 'transactions.id')
+      order(@order, 'transactions.id').
+      select("SUM(operations.amount) AS amount")
     if params[:category_id]
       operations = operations.joins(:title => :categories).where('categories.id = ?', params[:category_id])
     end
     # tobb szamla eseten a szumma az osszeg
     @operations = operations.includes([:transaction, {:title => [{:operations => :person}, :categories]}]).
-      select("*,SUM(operations.amount) AS amount")
-    # filters
+      select('operations.title_id', 'operations.account_id')
+
     @page_title << '/operations'
 
     respond_to do |format|
-      format.html do
-        offset = calc_offset_by_param_page
-        @operations = @operations.limit(page_limit).offset(offset)
-
-        rows = Operation.connection.exec_query("
-          SELECT date, balance
-          FROM (
-            SELECT date,
-                   (@balance := @balance + amount) - amount AS balance,
-                   @rn := @rn + 1 AS rn
-            FROM    (#{operations.select("#{@date_field} AS date, SUM(operations.amount) AS amount").to_sql}) original_view
-            JOIN    (SELECT @rn := 0, @balance := 0) i
-            ) counted
-          WHERE (rn - 1) MOD #{page_limit} IN (0,#{page_limit - 1})
-          ").rows
-        if rows.length.odd?
-          r = operations.select("#{@date_field} AS date, 0 AS balance").last
-          rows << [r[:date], r[:balance]]
+      format.html {
+        operations = operations.select("#{@date_field} AS date")
+        if @backward
+          pages_query = Operation.scoped.
+            select("MAX(date) AS date_start, MIN(date) AS date_end, COUNT(*) AS c").order('date DESC')
+        else
+          pages_query = Operation.scoped.
+            select("MIN(date) AS date_start, MAX(date) AS date_end, COUNT(*) AS c").order('date ASC')
         end
-        @intervals = rows.each_slice(2)
+
+        case params[:per_page]
+        when "week" then pages_query = pages_query.group("YEARWEEK(date, 1)")
+        when "month" then pages_query = pages_query.group("YEAR(date),MONTH(date)")
+        when "quarter" then pages_query = pages_query.group("YEAR(date),QUARTER(date)")
+        when "year" then pages_query = pages_query.group("YEAR(date)")
+        else pages_query = pages_query.group("FLOOR(@rn := @rn + 1 / #{page_limit + 1})").joins("JOIN (SELECT @rn := 0) i")
+        end
+        # itt pontosan ugyanaz a sorrend kell a lapozoban es a lapon, maskepp az egyenleg elcsuszik
+        if continuous_paging?
+          operations = operations.order(@order).order(:id)
+        end
+
+        pages_query = pages_query.select("SUM(amount) AS sum_amount")
+        pages_query = pages_query.from("(#{operations.to_sql}) AS all_operations")
+
+        with_balance_query = Operation.scoped.
+          joins('JOIN (SELECT @balance := 0) i').
+          select("date_start, date_end, (@balance := @balance + sum_amount) - sum_amount AS balance, c").
+          from("(#{pages_query.to_sql}) AS pages")
+        query = with_balance_query
+
+        rows = Transaction.connection.exec_query(query.to_sql).rows
+
+        @intervals = []
+        rows.each{|row| @intervals << [[row[0], row[2]], [row[1]]] }
 
         @this_interval = @intervals.to_a[params[:page].to_i || 0] || ['']
-      end
+
+        if continuous_paging?
+          offset = calc_offset_by_param_page
+          # XXX lehetne ugy, hogy idoablakot ad meg a hivatkozo es konvertaljuk
+          #offset = calc_offset_by_param_date
+          @operations = @operations.limit(page_limit).offset(offset)
+        else
+          if @backward
+            @operations = @operations.where("#{@date_field} BETWEEN ? AND ?", @this_interval[1][0], @this_interval[0][0])
+          else
+            @operations = @operations.where("#{@date_field} BETWEEN ? AND ?", @this_interval[0][0], @this_interval[1][0])
+          end
+        end
+      }
       format.csv {
         require 'csv'
         csv = CSV.generate do |l|
@@ -145,72 +174,93 @@ class ViewController < ApplicationController
         titles = @treasury.titles
       end
     end
+    case params[:sort]
+    when 'title'
+      @date_field = 'titles.date'
+      @backward = true
+    when 'transaction'
+      @date_field = 'transactions.date'
+      @backward = true
+    else
+      if params[:category_id]
+        @date_field = 'transactions.date'
+        @backward = true
+      else
+        @date_field = 'titles.date'
+        @backward = true
+      end
+    end
+
+    @order = @date_field.dup
+    @order << ' DESC' if @backward
 
     @titles = titles.includes(
       :categories, :transaction,
       :operations => { :account => :person },
       :party => { :account => :person }
-    )
+    ).order(@order)
 
-    case params[:sort]
-    when 'title'
-      @date_field = 'titles.date'
-      @order = @date_field+' DESC'
-    when 'transaction'
-      @date_field = 'transactions.date'
-      @order = @date_field+' DESC'
-    else
-      if params[:category_id]
-        @date_field = 'transactions.date'
-        @order = @date_field+' DESC'
-      else
-        @date_field = 'titles.date'
-        @order = @date_field+' DESC'
-      end
-    end
-    @titles = @titles.order(@order)
-
-    offset = calc_offset_by_param_page
-    @titles = @titles.limit(page_limit).offset(offset)
-
-    rows = Title.connection.exec_query("
-      SELECT date, balance
-      FROM (
-        SELECT date,
-               (@balance := @balance + amount) - amount AS balance,
-               @rn := @rn + 1 AS rn
-        FROM    (#{titles.joins(:transaction).order(@order).select("#{@date_field} AS date, titles.amount").to_sql}) original_view
-        JOIN    (SELECT @rn := 0, @balance := 0) i
-        ) counted
-      WHERE (rn - 1) MOD #{page_limit} IN (0,#{page_limit - 1})
-      ").rows
-    if rows.length.odd?
-      r = titles.joins(:transaction).order(@order).select("#{@date_field} AS date, 0 AS balance").last
-      rows << [r[:date], r[:balance]]
-    end
-    @intervals = rows.each_slice(2)
-
-    @this_interval = @intervals.to_a[params[:page].to_i || 0] || ['']
+    titles = titles.joins(:transaction)
 
     @page_title << '/titles'
     #XXX titles_by_month: .order('applied_business_id')
-    # filters
-    #    if params[:filter_year]
-    #      start_date = Date.parse("%d-01-01" % params[:filter_year])
-    #      end_date = Date.parse("%d-12-31" % params[:filter_year])
-    #      @titles = @titles.where("date BETWEEN ? AND ?", start_date, end_date)
-    #    else
-    #      d = @treasury.transactions.order('date').last.date - 60
-    #      f = "%d-%d-01" % [d.year, d.month]
-    #      @titles = @titles #.where("date >= ?", f)
-    #    end
     # XXX ez head-re szur; mikor hasznaljuk?
     #    if params[:account_id] and (params[:category_id] or  params[:treasury_id])
     #      @titles = @titles.where('transactions.account_id' => params[:account_id])
     #    end
-    #@titles = @titles.limit(20)
+
     respond_to do |format|
-      format.html
+      format.html {
+        titles = titles.select("#{@date_field} AS date, titles.amount")
+        if @backward
+          pages_query = Title.scoped.
+            select("MAX(date) AS date_start, MIN(date) AS date_end, COUNT(*) AS c").order('date DESC')
+        else
+          pages_query = Title.scoped.
+            select("MIN(date) AS date_start, MAX(date) AS date_end, COUNT(*) AS c").order('date ASC')
+        end
+
+        case params[:per_page]
+        when "week" then pages_query = pages_query.group("YEARWEEK(date, 1)")
+        when "month" then pages_query = pages_query.group("YEAR(date),MONTH(date)")
+        when "quarter" then pages_query = pages_query.group("YEAR(date),QUARTER(date)")
+        when "year" then pages_query = pages_query.group("YEAR(date)")
+        else pages_query = pages_query.group("FLOOR(@rn := @rn + 1 / #{page_limit + 1})").joins("JOIN (SELECT @rn := 0) i")
+        end
+        # itt pontosan ugyanaz a sorrend kell a lapozoban es a lapon, maskepp az egyenleg elcsuszik
+        if continuous_paging?
+          titles = titles.order(@order).order(:id)
+        end
+
+        pages_query = pages_query.select("SUM(amount) AS sum_amount")
+        pages_query = pages_query.from("(#{titles.to_sql}) AS all_titles")
+
+        with_balance_query = Title.scoped.
+          joins('JOIN (SELECT @balance := 0) i').
+          select("date_start, date_end, (@balance := @balance + sum_amount) - sum_amount AS balance, c").
+          from("(#{pages_query.to_sql}) AS pages")
+        query = with_balance_query
+
+        rows = Transaction.connection.exec_query(query.to_sql).rows
+
+        @intervals = []
+        rows.each{|row| @intervals << [[row[0], row[2]], [row[1]]] }
+
+        @this_interval = @intervals.to_a[params[:page].to_i || 0] || ['']
+
+        if continuous_paging?
+          offset = calc_offset_by_param_page
+          # XXX lehetne ugy, hogy idoablakot ad meg a hivatkozo es konvertaljuk
+          #offset = calc_offset_by_param_date
+          @titles = @titles.limit(page_limit).offset(offset)
+        else
+          if @backward
+            @titles = @titles.where("#{@date_field} BETWEEN ? AND ?", @this_interval[1][0], @this_interval[0][0])
+          else
+            @titles = @titles.where("#{@date_field} BETWEEN ? AND ?", @this_interval[0][0], @this_interval[1][0])
+          end
+        end
+      }
     end
   end
 
@@ -231,7 +281,6 @@ class ViewController < ApplicationController
       @treasury = @account.treasury
       return if needs_deeply_concerned(@treasury)
       @date_field = "transactions.date"
-      @order = @date_field
       @page_title << '/' << view_context.print_account(@account)
       if params[:category_id]
         @category = Category.find(params[:category_id])
@@ -248,67 +297,87 @@ class ViewController < ApplicationController
       return if needs_deeply_concerned(@treasury)
       transactions = @treasury.transactions
       @date_field = "transactions.date"
-      @order = @date_field+" DESC"
+      @backward = true
     end
     @page_title << '/transactions'
     if params[:unacked]
       transactions = transactions.where(supervised: false)
       @page_title << '/unacked'
     end
+    if params[:sort] == 'last_touch'
+      @date_field = "transactions.updated_at"
+      @backward = true
+      @show_balance = false
+    else
+      @latest_updated_transaction = transactions.order('updated_at DESC').order(:id).first
+    end
+
+    @order = @date_field.dup
+    @order << " DESC" if @backward
+
     @transactions = transactions.includes(
       :titles,
       :parties => {
         :account => :person,
         :titles => [:categories, :operations => { :account => :person }]
       },
-    )
-    case params[:sort]
-    when 'last_touch'
-      @date_field = "transactions.updated_at"
-      @order = @date_field+" DESC"
-    end
-    @transactions = @transactions.order(@order)
+    ).order(@order).order(:id)
 
-    unless @order == "transactions.updated_at DESC"
-      @latest_updated_transaction = transactions.order('updated_at DESC').first
-    end
-
-    unless @show_balance
-      rows = Transaction.connection.exec_query("
-        SELECT date
-        FROM (
-          SELECT date,
-                 @rn := @rn + 1 AS rn
-          FROM    (#{transactions.order(@order).select("#{@date_field} AS date").to_sql}) original_view
-          JOIN    (SELECT @rn := 0) i
-          ) counted
-        WHERE (rn - 1) MOD #{page_limit} IN (0,#{page_limit - 1})
-        ").rows
+    transactions = transactions.select("#{@date_field} AS date")
+    if @backward
+      pages_query = Transaction.scoped.
+        select("MAX(date) AS date_start, MIN(date) AS date_end, COUNT(*) AS c").order('date DESC')
     else
-      rows = Transaction.connection.exec_query("
-        SELECT date, balance
-        FROM (
-          SELECT date,
-                 (@balance := @balance + amount) - amount AS balance,
-                 @rn := @rn + 1 AS rn
-          FROM    (#{transactions.order(@order).select("#{@date_field} AS date, parties.amount").to_sql}) original_view
-          JOIN    (SELECT @rn := 0, @balance := 0) i
-          ) counted
-        WHERE (rn - 1) MOD #{page_limit} IN (0,#{page_limit - 1})
-        ").rows
+      pages_query = Transaction.scoped.
+        select("MIN(date) AS date_start, MAX(date) AS date_end, COUNT(*) AS c").order('date ASC')
     end
-    if rows.length.odd?
-      r = transactions.order(@order).select("#{@date_field} AS date, 0 AS balance").last
-      rows << [r[:date], r[:balance]]
+
+    case params[:per_page]
+    when "week" then pages_query = pages_query.group("YEARWEEK(date, 1)")
+    when "month" then pages_query = pages_query.group("YEAR(date),MONTH(date)")
+    when "quarter" then pages_query = pages_query.group("YEAR(date),QUARTER(date)")
+    when "year" then pages_query = pages_query.group("YEAR(date)")
+    else pages_query = pages_query.group("FLOOR(@rn := @rn + 1 / #{page_limit + 1})").joins("JOIN (SELECT @rn := 0) i")
     end
-    @intervals = rows.each_slice(2)
+    # itt pontosan ugyanaz a sorrend kell a lapozoban es a lapon, maskepp az egyenleg elcsuszik
+    if continuous_paging?
+      transactions = transactions.order(@order).order(:id)
+    end
+
+    if @show_balance
+      transactions = transactions.select('parties.amount')
+      pages_query = pages_query.select("SUM(amount) AS sum_amount")
+    end
+    pages_query = pages_query.from("(#{transactions.to_sql}) AS all_transactions")
+    if @show_balance
+      with_balance_query = Transaction.scoped.
+        joins('JOIN (SELECT @balance := 0) i').
+        select("date_start, date_end, (@balance := @balance + sum_amount) - sum_amount AS balance, c").
+        from("(#{pages_query.to_sql}) AS pages")
+      query = with_balance_query
+    else
+      query = pages_query
+    end
+
+    rows = Transaction.connection.exec_query(query.to_sql).rows
+
+    @intervals = []
+    rows.each{|row| @intervals << [[row[0], row[2]], [row[1]]] }
 
     @this_interval = @intervals.to_a[params[:page].to_i || 0] || ['']
 
-    offset = calc_offset_by_param_page
-    # XXX lehetne ugy, hogy idoablakot ad meg a hivatkozo es konvertaljuk
-    #offset = calc_offset_by_param_date
-    @transactions = @transactions.limit(page_limit).offset(offset)
+    if continuous_paging?
+      offset = calc_offset_by_param_page
+      # XXX lehetne ugy, hogy idoablakot ad meg a hivatkozo es konvertaljuk
+      #offset = calc_offset_by_param_date
+      @transactions = @transactions.limit(page_limit).offset(offset)
+    else
+      if @backward
+        @transactions = @transactions.where("#{@date_field} BETWEEN ? AND ?", @this_interval[1][0], @this_interval[0][0])
+      else
+        @transactions = @transactions.where("#{@date_field} BETWEEN ? AND ?", @this_interval[0][0], @this_interval[1][0])
+      end
+    end
 
     respond_to do |format|
       format.html do
@@ -347,10 +416,19 @@ class ViewController < ApplicationController
   end
 
   def page_limit
-    if params[:per_page]
+    if params[:per_page].nil?
+      50
+    elsif params[:per_page].to_i.nonzero?
       params[:per_page].to_i
     else
-      50
+      params[:per_page]
     end
+  end
+
+  def discrete_paging?
+    not params[:per_page].nil? and params[:per_page].to_i.zero?
+  end
+  def continuous_paging?
+    params[:per_page].nil? or params[:per_page].to_i.nonzero?
   end
 end
