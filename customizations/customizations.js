aed2df2d9fa132c60167f05ddd45e2af9ae4a262
Roland Pallai <pallair@magex.hu>
2015-11-06 14:25:57 +0100

    customizations: javascript tamogatas
    Peldaul kapcsolodhatnak a html template-ekhez.

---
+++ customizations/customizations.js
@@ -0,0 +1,10 @@
+// This is a manifest file that'll be compiled into customizations.js, which will include all the files
+// listed below.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require_tree .
